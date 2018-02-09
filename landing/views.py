4130097049bdc4fe2b88b371376e28e013ddd70e
halvorboe <halvor98@outlook.com>
2018-02-09 21:38:26 +0100

    Beautify

--- landing/views.py
+++ landing/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from news.models import Article
+from events.models import Event
 
 
 def index(request):
-	return render(request, 'landing/index.html', {'articles': Article.objects.all()})
+	return render(request, 'landing/index.html', {'articles': Article.objects.all(), 'events': Event.objects.all()})
