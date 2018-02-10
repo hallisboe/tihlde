604156dbfe841ed2c954c4702927ed6ffdee87b0
halvorboe <halvor98@outlook.com>
2018-02-10 15:14:08 +0100

    cool

--- events/forms.py
+++ events/forms.py
@@ -8,3 +8,19 @@ class ParticipantForm(ModelForm):
 	class Meta:
 		model = Participant
 		fields = ('name', 'phone', 'email', 'allergies', 'program', 'grade')
+		labels = {
+			'name': 'Navn',
+			'phone': 'Telefon', 
+			'email': 'Epost', 
+			'allergies': 'Allergier', 
+			'program': 'Studie', 
+			'grade': 'Trinn',
+		}
+
+
+	def __init__(self, *args, **kwargs):
+		super(ParticipantForm, self).__init__(*args, **kwargs)
+		for field in iter(self.fields):
+			self.fields[field].widget.attrs.update({
+			'class': 'form-control'
+			})
