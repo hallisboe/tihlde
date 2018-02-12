f493a43873b8f087caa596a6953df323c89e20fb
halvorboe <halvor98@outlook.com>
2018-02-12 16:53:16 +0100

    added url and html shit

--- landing/urls.py
+++ landing/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 landing_patterns = ([
     path('', views.index, name='list'),
+    path('om-oss/', views.about, name='about'),
+    path('ny-student/', views.intro, name='intro'),
+    path('for-bedrifter/', views.promo, name='promo'),
+    path('tjenester/', views.services, name='services'),
+    path('frivillige/', views.volenteers, name='volenteers'),
 ], 'landing')
--- landing/views.py
+++ landing/views.py
@@ -8,3 +8,18 @@ from .models import Post
 
 def index(request):
 	return render(request, 'landing/index.html', {'posts': Post.objects.all().order_by('-published'), 'events': Event.objects.all().order_by('start')[:6]})
+
+def about(request):
+	return render(request, 'landing/about.html', {})
+
+def intro(request):
+	return render(request, 'landing/intro.html', {})
+
+def promo(request):
+	return render(request, 'landing/promo.html', {})
+
+def services(request):
+	return render(request, 'landing/services.html', {})
+
+def volenteers(request):
+	return render(request, 'landing/volenteers.html', {})
